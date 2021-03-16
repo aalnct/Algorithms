d8561fd9e1db08bd21b768c53fd8759eebbb85b4
Amit Agarwal <amit.agarwal@searshc.com>
2021-03-15 22:59:09 -0500

    365 days of coding

---
+++ src/fb/LetterCasePermutation.java
@@ -0,0 +1,41 @@
+package fb;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LetterCasePermutation {
+
+    public static void main(String[] args) {
+        String string = "a1b2";
+        List<String> result = letterCasePermutation(string);
+        result.stream().forEach(s -> System.out.println());
+    }
+
+    static List<String> letterCasePermutation (String string) {
+        Set<String> result = new HashSet<>();
+        if (string.length() <= 0) {
+            return null;
+        }
+
+        helper(new char[string.length()], result,0,string);
+
+        return new ArrayList<>(result);
+    }
+
+    private static void helper(char[] chars, Set<String> result, int position, String string) {
+        if (position == chars.length) {
+            result.add(new String(chars));
+            return;
+        }
+
+        chars[position] = Character.toUpperCase(string.charAt(position));
+        helper(chars,result,position+1,string);
+
+        chars[position] = Character.toLowerCase(string.charAt(position));
+        helper(chars,result,position+1,string);
+    }
+
+
+}
